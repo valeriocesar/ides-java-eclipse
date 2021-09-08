aef30d7585e7e075dd3c5806fad18ad59987c109
valeriocesar <valerio.cesar@gmail.com>
2021-09-08 13:42:45 -0300

    Firts commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,23 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato(null, null, null);
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World! " + (a+b));*/
+	}
+
+}
+
+class Livros {
+}
